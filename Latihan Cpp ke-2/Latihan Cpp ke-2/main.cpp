55a6791f6ed01eac8f636aa8626b3ec2fcfaa87c
Sasongko Adi <sasongkoadi005@gmail.com>
2016-11-22 17:58:58 +0800

    Latihan CPP ke-2

---
+++ Latihan Cpp ke-2/Latihan Cpp ke-2/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  Latihan Cpp ke-2
+//
+//  Created by Sasongko Adi on 22/11/16.
+//  Copyright © 2016 Sasongko Adi. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+ 
+    int a, b;
+    
+    cout << "Enter Number 1 = ";
+    cin >> a;
+    
+    cout << "Enter Number 2 = ";
+    cin >> b;
+    
+    
+    if (a>b) {
+        cout << a <<" > "<<b<< endl;
+    } else {
+        cout << a<< " < " << b << endl;
+    }
+    
+    
+}
