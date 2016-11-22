7bb01dc78a881f17241894425c8c8fd5fa029263
Sasongko Adi <sasongkoadi005@gmail.com>
2016-11-22 23:58:08 +0800

    test.cpp

---
+++ test.cpp
@@ -0,0 +1,29 @@
+int main(int argc, const char * argv[]) {
+    } else {
+        cout << a<< " < " << b << endl;
+    }
+    */
+    
+    var_1=a >= b;
+    
+    var_2=a <= b;
+    
+    
+    cout << "Enter Number A = ";
+    cin >> a;
+    
+    cout << "Enter Number B = ";
+    cin >>b;
+    
+    
+    
+    
+    if (var_1 || var_2) {
+        cout << var_1 << " || " << var_2 << " = True "<< endl;
+    } else {
+        cout << var_1 << " || " << var_2 << " = False "<< endl;
+    }
+    
+    
+    
+}
