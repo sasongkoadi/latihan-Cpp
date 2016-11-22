00da72321d4bc991f70ce13ee5f59bf652658b0a
Sasongko Adi <sasongkoadi005@gmail.com>
2016-11-23 00:03:19 +0800

    Latihan fungsi

--- Latihan Cpp ke-2/Latihan Cpp ke-2/main.cpp
+++ Latihan Cpp ke-2/Latihan Cpp ke-2/main.cpp
@@ -12,7 +12,9 @@ using namespace std;
 int main(int argc, const char * argv[]) {
  
     int a, b;
+    int var_1, var_2;
     
+    /*
     cout << "Enter Number 1 = ";
     cin >> a;
     
@@ -25,6 +27,28 @@ int main(int argc, const char * argv[]) {
     } else {
         cout << a<< " < " << b << endl;
     }
+    */
+    
+    var_1=a >= b;
+    
+    var_2=a <= b;
+    
+    
+    cout << "Enter Number A = ";
+    cin >> a;
+    
+    cout << "Enter Number B = ";
+    cin >>b;
+    
+    
+    
+    
+    if (var_1 || var_2) {
+        cout << var_1 << " || " << var_2 << " = True "<< endl;
+    } else {
+        cout << var_1 << " || " << var_2 << " = False "<< endl;
+    }
+    
     
     
 }
